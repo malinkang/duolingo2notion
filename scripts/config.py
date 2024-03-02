36643f5999355a4d408a61cb5a2af8226018f336
malinkang <malinkang@adsconflux.com>
2024-03-02 15:15:26 +0800

    first commit

---
+++ scripts/config.py
@@ -0,0 +1,19 @@
+
+RICH_TEXT = "rich_text"
+URL = "url"
+RELATION = "relation"
+NUMBER = "number"
+DATE = "date"
+FILES = "files"
+STATUS = "status"
+TITLE = "title"
+SELECT = "select"
+MULTI_SELECT = "multi_select"
+
+workout_properties_type_dict = {
+    "标题":TITLE,
+    "经验":NUMBER,
+    "学习时长":NUMBER,
+    "单元":NUMBER,
+    "日期":DATE,
+}
